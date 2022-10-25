890fa8ca9d4561aaee37b6654062956beef7e972
Diego Nahuel Saldaña Rodriguez <diegonsaldana@gmail.com>
2022-10-25 20:02:39 -0300

    Agregue el Sobre Nosotros

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 //import logo from "../public/logo.svg"
 import NavBar from "./componentes/dev-edmen/NavBar";
-
+import SobreNosotros from "./componentes/SobreNosotros/SobreNosotros.js"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <SobreNosotros/>
     </div>
   );
 }
---
+++ src/componentes/SobreNosotros/SobreNosotros.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./SobreNosotros.css"
+
+function SobreNosotros() { 
+    return (
+    <div className="sobreNos">
+    <div>
+        <h3 className="titulo"> BURGUER HOUSE </h3>
+        <img className="imagen" src="../../../logo.jpg" alt="icono"/>
+        
+    </div>
+    <div className="text"> 
+        <p> En <strong>BURGUER HOUSE</strong> reinventamos la hamburguesa.
+            Reformulamos la receta original, desarrollando hamburguesas gourmet, llenas de sabor, 
+            de estilo casero, usando los mejores ingredientes frescos y naturales. Presentadas en un 
+            pan casero especial de elaboración propia. Tenemos una propuesta innovadora de Pop-up 
+            Stores montados en contenedores reciclados, totalmente equipados, que prometen al visitante 
+            una experiencia gastronómica única, garantizando la calidad y la originalidad de nuestros 
+            productos.
+            <h3>Estamos en C.A.B.A y G.B.A</h3>
+            Te esperamos en cualquiera de nuestras sucursales para disfrutar    
+            de las mejores hamburguesas. Para Delivery podes hacer tu pedido en la web o encontranos en 
+            Pedidos Ya y Rappi.
+        </p>
+    </div>
+</div>
+    )
+}
+
+export default SobreNosotros;
