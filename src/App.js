213dd6c2a78f08412b07d2655b8616f7557eb79f
edmenx <edmenx@gmail>
2022-10-29 14:28:19 -0300

    Cambios en src

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-//import logo from "../public/logo.svg"
 import NavBar from "./componentes/dev-edmen/NavBar";
 
-
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+   return (
+      <div className="App">
+         <NavBar />
+      </div>
+   );
 }
 
 export default App;
