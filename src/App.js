65a8908f8feb545256d39ced3b946257492431a5
NadiaAnabellaRapp <nadia.a.rapp@gmail.com>
2022-11-08 19:21:22 -0300

    Footer versión 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-<<<<<<< HEAD
-import Footer from "./Componentes/Footer";
-=======
+
+import Footer from "./componentes/Footer/Footer";
 import Banner from "./componentes/Banner/Banner" 
 import NavBar from "./componentes/dev-edmen/NavBar";
 import SobreNosotros from "./componentes/SobreNosotros/SobreNosotros.js"
->>>>>>> 0ee2e58f2bc80f7b2ad9d477c50bed471a2d9289
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <Footer />
-=======
 
       <NavBar />
       <Banner/>
       <SobreNosotros/>
 
->>>>>>> 0ee2e58f2bc80f7b2ad9d477c50bed471a2d9289
     </div>
   );
 }
